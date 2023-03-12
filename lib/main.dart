07b3792674ee56442d5c3a6b6b1960cbf74714f2
Ruslan <volna4753@gmail.com>
2023-03-12 17:33:19 +0300

    Познакоимились с Виджетом "Scaffold" и как с помощью него можно создавать текст и кнопку и изменять выравнивание и расположение элементов

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp (
-    // Главный экран -> текстовый виджет
-    home: Text("hello, man"),
+    // Главный экран -> Scaffold виджет
+    home: Scaffold(
+      // Верхний бар
+      appBar: AppBar(
+        title: Text("Learning Flutter"),
+        // централизуем текст
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("hello man"),
+      ),
+      floatingActionButton: FloatingActionButton( // Кнопочка с текстом
+          onPressed: () {  },
+          child: Text("Click")
+      ),
+    ),
   ));
 
