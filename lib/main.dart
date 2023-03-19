91c540b4ee365560c7299566f4da8614c4ec3838
Ruslan <volna4753@gmail.com>
2023-03-19 17:24:51 +0300

    Урок 15.
    Создаём новый учебный проект прямо тут, уже зная основы основ
    
    1) Создали аппбар, в котором убрали тень
    2) Задали фон
    3) Задали корневой виджет паддинга для дочерних виджетов
    4) Создали текстовые виджеты и добавили им свойств
    5) Использовали SizedBox для того, чтобы увеличить расстояние между виджетами
    6) Добавили иконку и email
    7) Добавили круглую автарку
    8) Сделали Divider (разделить) между аваркой и всем остальным

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,102 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp (
-    // Вызываем экран Home
-    home: Home()
+    home: NinjaCard()
   ));
 
 
-// Виджет без состояния (не изменяемый)
-// Используется для автоматической горячей перезагрузки,
-// после внесённых изменений, а так же возвращает
-// экран Home
-// stless - быстрый шаблон
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return
-      // Главный экран -> Scaffold виджет
       Scaffold(
-        // Верхний бар
+        // Задаёт общий фон
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: const Text("Learning Flutter"),
-          // централизуем текст
+          title: Text('Ninja ID Card'),
           centerTitle: true,
-          backgroundColor: Colors.red[600],// При нажатии ctrl + q можно посмотреть палитру цветов
+          backgroundColor: Colors.grey[850],
+          // Убирает тень на appbar
+          elevation: 0.0,
         ),
-        body: Row(
-          children: <Widget>[
-            Expanded(
-                child: Image.asset('assets/1.jpg'),
-              flex: 3,
-            ),
-            // Виджет Expanded растягивает контейнер по всей доступной ширине
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.cyan,
-                child: Text('1'),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/1.jpg'),
+                  radius: 40.0,
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.pinkAccent,
-                child: Text('2'),
+              // Разделитель
+              Divider(
+                height: 60.0,
+                color: Colors.grey[850],
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.amber,
-                child: Text('3'),
+              Text(
+                  'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  // Интервал между буквами
+                  letterSpacing: 2.0
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 10.0,), // Коробка используется для того, чтобы увеличить расстояние между виджетами
+              Text(
+                'Chort',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    // Интервал между буквами
+                    letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                    color: Colors.grey,
+                    // Интервал между буквами
+                    letterSpacing: 2.0
+                ),
+              ),
+              SizedBox(height: 10.0,), // Коробка используется для того, чтобы увеличить расстояние между виджетами
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    // Интервал между буквами
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0,),
+                  Text(
+                    'Test1234@gmail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton( // Кнопочка с текстом
-          onPressed: () {},
-          child: const Text("Click"),
-          backgroundColor: Colors.red[600],
-        ),
-    );
+      );
   }
 }
