6227fa7499992e156cc4988656afda246f0b814c
Ruslan <volna4753@gmail.com>
2023-03-21 21:19:29 +0300

    Урок 22.
    И снова новое приложение. Тут будет 3 экрана
    
    1) Сделали 3 экрана: home, choose_location и loading
    2) Добавили виджет SafeArea, который помогает от скрытия сверху
    3) Сделали вывод текст на каждом экране/виджете

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,9 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote-card.dart';
+import 'package:kekaga/pages/home.dart';
+
 
 void main() => runApp(MaterialApp (
-    home: QuoteList(),
+    home: Home(),
   ));
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  // Используя класс Quote, который мы добавили объявляем цитату и автора
-  List<Quote> quotes = [
-  Quote(text: 'Life is short. Smile while you still have teeth.', author: 'author1'),
-  Quote(text: 'Live each day as if it’s your last.', author: 'author2'),
-  Quote(text: 'Life is beautiful. Enjoy the ride.', author: 'author3')
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        // будет просматривать список данных из списка
-        // по сути в скобках после map сделали функцию, которая возвращает наш список цитат
-        // теперь QuoteCard(quote: quote) задаёт функцию, которая вернёт карточки
-        children: quotes.map((quote) => QuoteCard(
-            quote: quote,
-            delete: () {
-              // Устанавливаем состояние
-              setState(() {
-                quotes.remove(quote);
-              });
-            },
-        )).toList(), // говорим, что будем просматривать список
-      ),
-    );
-  }
-}
-
 
 
