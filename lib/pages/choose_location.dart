b2cd9bedd3efb740ff5135d1fbe7abd79e61a5ed
Ruslan <volna4753@gmail.com>
2023-03-22 21:29:04 +0300

    Урок 25.
    Про асинхронный код
    Асинхронный код - действие, которые начинается сейчас, а заканчивается когда-нибудь в будующем (например долгий запрос)
    
    1) Делаем функцию getData, которая ничего не будет делать, но использоваться в примере с асинхронным кодом
    2) Вызываем функцию getData в функции initState
    Благодаря await мы заставляем джать функцию, которая выполняется после, пока предыдущая завершится.
    Без await следующая функция не будет ждать выполнения предыдущей

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,12 +9,35 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  int counter = 0;
+  void getData () async {
+    // Моделируем сетевой запрос
+    // Делаем задержку, длительностью 3 сек и функцию, которая срабатывает спустя эти 3 секунды
+    await Future.delayed(Duration(seconds: 3),(){
+      print('Deleay 3 sec Done');
+    });
 
+    // теперь это говно заставит ждать getData, но насрёт на print в initState()
+    // Моделируем сетевой запрос
+    // Делаем задержку, длительностью 3 сек и функцию, которая срабатывает спустя эти 3 секунды
+    String username = await Future.delayed(Duration(seconds: 4),(){
+      return 'Kirill!!\n Deleay 4 sec Done';
+    });
+
+    // теперь это говно будет выполняться только по предыдущего
+    // Моделируем сетевой запрос
+    // Делаем задержку, длительностью 3 сек и функцию, которая срабатывает спустя эти 3 секунды
+    String bio =  await Future.delayed(Duration(seconds: 2),(){
+      return ' <\nбиография !\nАсихронно Deleay 2 sec Done';
+    });
+
+    // Пока обе переменные не будут возвращены, ни одна не распечается
+    print('$username - $bio \nWithout deleay');
+  }
+
+  int counter = 0;
 
   // Крч эта функция определяет метод initState и вызывается
   // в момент добавления виджета на экран
-
   //@override указывает, что функция переопределяет метод initState()
   @override
   void initState(){
@@ -22,7 +45,11 @@ class _ChooseLocationState extends State<ChooseLocation> {
     // из базового класса State, который обычно выполняет основную
     // инициализацию состояния виджета.
     super.initState();
+    // Вызываем функцию getData, которая имитирует задержку
+    getData();
+    // print не блокируется, а выполняется, как можно быстрее
     print("initState function ran");
+
   }
 
   @override
