e82746a721b58dd6c4ace122996a05075a2c4aaa
Ruslan <volna4753@gmail.com>
2023-03-22 20:50:54 +0300

    Урок 24.
    20 строка
    initState() - вызывается один раз при создании виджета
    
    30 строка
    Build() - строится с деревом виджета.
    Вызывается, когда меняется состояние приложения
    
    Не используется
    Dispose() - вызывается, когда виджет или состояние объекта удаляется
    
    1) Сделали функцию определяющую метод initState и вызывается в момент добавления виджета на экран
    2) Сделали кнопку с счётчиком.
    При каждом вызове этой функции это логируется в консоль

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,26 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+
+  // Крч эта функция определяет метод initState и вызывается
+  // в момент добавления виджета на экран
+
+  //@override указывает, что функция переопределяет метод initState()
+  @override
+  void initState(){
+    //Строка super.initState() вызывает родительский метод initState()
+    // из базового класса State, который обычно выполняет основную
+    // инициализацию состояния виджета.
+    super.initState();
+    print("initState function ran");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("Build function ran");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,7 +36,13 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose location screen'),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter +=1;
+          });
+        }, child: Text('Counter is $counter'),
+      ),
     );
   }
 }
