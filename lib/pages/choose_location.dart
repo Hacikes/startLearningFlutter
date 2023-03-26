e8f1f8b61c57e716a41f2fbc23a2d1851f1847be
Ruslan <volna4753@gmail.com>
2023-03-26 21:12:27 +0300

    Урок 34.
    Делаем выбор локаций на экране локаций
    
    1) Импортировали класс WorldTime на экран choose_location.dart
    2) Добавили список стран на выбор с url и картинками
    3) Сделали список, который можно тыкать и выбирать страну( циклически выбирается строка по индексу начиная от 0)
    4) Настроили загрузку флагов для каждой локации
    5) Для списка сделали действие при тапе - вывод в консоль названия локации

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kekaga/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key? key}) : super(key: key);
@@ -11,6 +12,15 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   int counter = 0;
 
+  // Наши варианты выбора местоположения
+  // Берём класс WorldTime из файла world_time.dart
+  List<WorldTime> locations = [
+  WorldTime(location: 'Moscow', flag: 'Russia_flag.jpg', url: 'Europe/Moscow'),
+  WorldTime(location: 'Berlin', flag: 'germaniya.jpg', url: 'Europe/Berlin'),
+  WorldTime(location: 'London', flag: 'English.jpg', url: 'Europe/London'),
+  WorldTime(location: 'Warsaw', flag: 'Poland.jpg', url: 'Europe/Warsaw')
+  ];
+
   @override
   Widget build(BuildContext context) {
     print("Build function ran");
@@ -22,12 +32,27 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter +=1;
-          });
-        }, child: Text('Counter is $counter'),
+      body: ListView.builder(
+          itemCount: locations.length,
+          // Будет циклически просматривать список
+          itemBuilder: (context, index){
+            // Возвращаем список, на который можно будет жмякать
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    print(locations[index].location);
+                  },
+                  title: Text(locations[index].location),
+                  // Загружаем изображения флагов
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          },
       ),
     );
   }
