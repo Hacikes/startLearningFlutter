89aaa6225488c42458b8bf0a809b7c6a89663195
Ruslan <volna4753@gmail.com>
2023-03-26 18:39:38 +0300

    Урок 29.
    Делаем обработчик ошибок
    try catch
    
    1) Сделали конструкцию try catch в файле world_time.dart
    2) Сделали специально ошибку в url и сделали вывод ошибки в консоль
    3) И сделали сообщение ошибки для ui

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -13,27 +13,46 @@ class WorldTime{
 
   // Делаем асинхронную функцию, которая не блокирует использование приложения, пока она работает
   Future <void> getTime () async {
-    // Делаем запрос на получение времени
-    Response response = await get(Uri.parse('https://www.timeapi.io/api/Time/current/zone?timeZone=$url'));
-    Map data = jsonDecode(response.body);
 
-    // Извлекаем данные
-    String dateTime = data['dateTime'];
-    // Отличие во времени +03:00 типо того
-    // substring выбирает только промежуток символов из того, что пришло
-    String timeZone = data['timeZone'].substring(7,13);
+    // обрабочик ошибок try catch
+    // пытаемся выполнить запрос и если не получится, то выполняется блок catch
+    try {
+      // Делаем запрос на получение времени
+      Response response = await get(Uri.parse('https://www.timeapi.io/api/Time/current/zone?timeZone=$url'));
+      Map data = jsonDecode(response.body);
 
-    //String nowLocation = timeZone;
+      // Извлекаем данные
+      String dateTime = data['dateTime'];
+      // Отличие во времени +03:00 типо того
+      // substring выбирает только промежуток символов из того, что пришло
+      String timeZone = data['timeZone'].substring(7,13);
 
-    // Создать объект даты и локации
-    DateTime now = DateTime.parse(dateTime);
-    //Строка ниже могла бы превратить строку, которую мы получили в число
-    // Закомментил, так как нам не надо переводить переменную timeZone
-    // в int, т.к. она и должна быть String
-    //now = now.add(Duration(hours: int.parse(timeZone)));
+      //String nowLocation = timeZone;
 
-    // Задаём свойство time
-    time = now.toString();
+      // Создать объект даты и локации
+      DateTime now = DateTime.parse(dateTime);
+      //Строка ниже могла бы превратить строку, которую мы получили в число
+      // Закомментил, так как нам не надо переводить переменную timeZone
+      // в int, т.к. она и должна быть String
+      //now = now.add(Duration(hours: int.parse(timeZone)));
+
+      // Задаём свойство time
+      time = now.toString();
+    }
+    // e - переменная, которая ловит ошибки и сохраняет их у себя
+
+    catch(e){
+      // В данном случае выведется ошибка в консоль
+      print('ERORR: $e');
+      // Устанавливаем перенной значение, дабы не выводилась всякая бурда
+      // так как не удалось получить карту из ответа, мы сами обозначем,
+      // чему будет равна переменная time в случае ошибки
+      time = 'Не удалось получить данные о времени';
+    }
+
+
+
+
   }
 }
 
